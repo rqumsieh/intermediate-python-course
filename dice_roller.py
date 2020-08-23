8164b3b3fb33298de50dcfd1ee46e85df54862de
Robin Qumsieh <robinqumsieh@Robins-MacBook-Pro.local>
2020-08-23 14:37:04 -0700

    fixing loop for dice rolls + added players and dictionary to store dice sums

--- dice_roller.py
+++ dice_roller.py
@@ -1,18 +1,25 @@
 def main():
   import random
-  dice_rolls = int(input('How many dice would you like to roll? '))
-  dice_sum = 0
-  dice_size = int(input('How many sides are the dice? '))
-  for i in range(0,dice_size):
-    roll = random.randint(1,dice_size)
-    dice_sum += roll
-    if roll == 1:
-        print(f'You rolled a {roll}! Critical Fail')
-    elif roll == dice_size:
-        print(f'You rolled a {roll}! Critical Success!')
-    else:
-        print(f'You rolled a {roll}')
-  print(f'You have rolled a total of {dice_sum}')
+  num_of_players = int(input('How many players?'))
+  player_totals=dict()
+  for j in range(0, num_of_players):
+      name = input('Hello! What is your name?')
+      print('Welcome ' +name+'!')
+      dice_rolls = int(input('How many dice would you like to roll? '))
+      dice_sum = 0
+      dice_size = int(input('How many sides are the dice? '))
+      for i in range(0,dice_rolls):
+        roll = random.randint(1,dice_size)
+        dice_sum += roll
+        if roll == 1:
+            print(f'You rolled a {roll}! Critical Fail')
+        elif roll == dice_size:
+            print(f'You rolled a {roll}! Critical Success!')
+        else:
+            print(f'You rolled a {roll}')
+      print(f'You have rolled a total of {dice_sum}')
+      player_totals[name] = dice_sum
+  print(player_totals)
 
 if __name__== "__main__":
   main()
